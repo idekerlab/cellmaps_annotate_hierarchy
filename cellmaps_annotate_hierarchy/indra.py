c3e40c1e8f9f00676210d1c9e5f5ea1dbe5cd521
Dexter Pratt <depratt@ucsd.edu>
2023-05-01 15:11:12 -0400

    Add function to summarize statements
    Added the ChatGPT-generated function to summarize statements. Here is the prompt I used:
    
    The following code generates a datastructure of ranked statements from INDRA. write a function that will take the top 10 statements, concatenate their text, and query ChatGPT to summarize them, where the prompt specifies that the agent should act as an assistant to a molecular biologist, performing critical analysis of protein interactions and the response should be returned as a JSON structure with a separating the analysis and the relevant citations. <code for query_indra and sort_response_by_relationship_type>

--- cellmaps_annotate_hierarchy/indra.py
+++ cellmaps_annotate_hierarchy/indra.py
@@ -96,8 +96,54 @@ def sort_response_by_relationship_type(statements):
 
     return ranked_statements
 
+import requests
+import json
+
+def summarize_top_statements(statements, top_n=10):
+    # Concatenate the text of the top 10 statements
+    texts = [statement['text'] for statement in statements[:top_n]]
+    concatenated_texts = ' '.join(texts)
+
+    # Prepare the GPT prompt
+    prompt = f"As an assistant to a molecular biologist, perform critical analysis of the following protein interactions: {concatenated_texts}"
+
+    # Query the ChatGPT API
+    api_url = "https://api.openai.com/v1/engines/davinci-codex/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
+    }
+    data = {
+        "prompt": prompt,
+        "max_tokens": 150,
+        "n": 1,
+        "stop": None,
+        "temperature": 0.7,
+    }
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        result = response.json()["choices"][0]["text"].strip()
+        analysis_and_citations = {
+            "analysis": result,
+            "citations": [statement["evidence"][0]["source_api"] for statement in statements[:top_n]]
+        }
+        return analysis_and_citations
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        return None
 
 '''
+# Example usage
+if __name__ == "__main__":
+    agent_a = "BRCA1"
+    agent_b = "TP53"
+    statements = query_indra(agent_a, agent_b)
+    ranked_statements = sort_response_by_relationship_type(statements)
+    analysis_and_citations = summarize_top_statements(ranked_statements)
+    print(json.dumps(analysis_and_citations, indent=2))
+
+# earlier code, temporarily preserved for reference
 def get_indra_pairwise_interactions(proteins, limit=50):
     pairwise_interactions = {}
     for agent_a, agent_b in combinations(proteins, 2):
